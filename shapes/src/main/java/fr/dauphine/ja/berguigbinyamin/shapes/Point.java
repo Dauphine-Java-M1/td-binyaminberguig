740080ea6993d0f9d70dfb6fb83536a7137dd2e4
bergbi18 <bergbi18@b032-21.crio.dauphine.fr>
2019-10-11 14:42:39 +0200

    Fin de l'exercice 2

---
+++ shapes/src/main/java/fr/dauphine/ja/berguigbinyamin/shapes/Point.java
@@ -0,0 +1,34 @@
+package fr.dauphine.ja.berguigbinyamin.shapes;
+
+import java.util.ArrayList;
+
+public class Point {
+
+	private int x,y;
+	
+	public Point(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+	
+	public Point(Point pt){
+		this.x=pt.x;
+		this.y=pt.y;
+	}
+	
+	public boolean isSameAs(Point pt) {
+		return this.x==pt.x && this.y==pt.y;
+	}
+	
+    public static void main( String[] args )
+    {
+    	Point p1=new Point(1,2);
+    	Point p2=p1;
+    	Point p3=new Point(1,2);
+    	ArrayList<Point> list = new ArrayList<Point>();
+    	list.add(p1);
+    	System.out.println(p1.isSameAs(p2));
+    	System.out.println(p1.isSameAs(p3));
+    }
+	
+}
---
+++ shapes/src/main/java/fr/dauphine/ja/berguigbinyamin/shapes/TestPoint.java
@@ -0,0 +1,13 @@
+package fr.dauphine.ja.berguigbinyamin.shapes;
+
+public class TestPoint {
+	private int x,y;
+	
+
+	
+    public static void main( String[] args )
+    {
+    	Point p=new Point();
+    	System.out.println(p.x+" "+p.y);
+    }
+}
